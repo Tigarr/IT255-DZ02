2bde409a817205c3fd044af134d7d193f1225dc7
Tigarr <sibirskitigarsibirski@gmail.com>
2020-08-24 15:01:15 +0200

    Drugi domaci

---
+++ IT255-DZ02-NebojsaBosijok3440/src/Main.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+
+
+
+/**
+ *
+ * @author Nesa
+ */
+public class Main {
+
+    
+    public static void main(String[] args) throws IOException {
+        System.out.println("---------->PREZENTACIJA GET METODE<----------");
+    GetAndPost.MyGETRequest();
+        System.out.println("---------->PREZENTACIJA POST METODE<----------");
+    GetAndPost.MyPOSTRequest();
+}
+
+    
+    
+}
